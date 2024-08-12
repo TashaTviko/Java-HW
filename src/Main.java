2aa338ef237c840190f8b4302f31ceb6a00ba597
Natalia Korobchenko <tasha.tviko@gmail.com>
2024-08-12 19:11:55 +0300

    add program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int price = 9000;
+                int bonus = 20;
+                        int miles = price/ bonus;
+        System.out.println("Количество бонусных миль:");
+        System.out.println(miles);
     }
 }
