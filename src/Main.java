f243c08c9ac093b11a26b192e23065e94084aa29
Natalia Korobchenko <tasha.tviko@gmail.com>
2024-08-12 19:20:28 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price = 9000;
-                int bonus = 20;
-                        int miles = price/ bonus;
+        int price = 9000; // стоимость билета
+        int bonus = 20; // количество рублей для одной бонусной мили
+        int miles = price / bonus;
         System.out.println("Количество бонусных миль:");
         System.out.println(miles);
     }
